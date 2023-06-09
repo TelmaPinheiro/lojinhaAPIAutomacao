bac51738bd1c6f12d99a26956dbee02831fc9f8e
Telma Pinheiro <telmapy@yahoo.com.br>
2023-06-09 14:51:10 -0300

    Inicializando o repositorio com os arquivos iniciais do projeto

---
+++ src/test/java/pojo/ProdutoPojo.java
@@ -0,0 +1,56 @@
+package pojo;
+
+import java.util.List;
+
+public class ProdutoPojo {
+    private String produtoNome;
+    private double produtoValor;
+    private List<String> produtoCores;
+    private String produtoUrlMock;
+    private List<ComponentePojo> compoentes;
+
+    public String getProdutoNome() {
+        return produtoNome;
+    }
+
+    public void setProdutoNome(String produtoNome) {
+        this.produtoNome = produtoNome;
+    }
+
+    public double getProdutoValor() {
+        return produtoValor;
+    }
+
+    public void setProdutoValor(double produtoValor) {
+        this.produtoValor = produtoValor;
+    }
+
+    public List<String> getProdutoCores() {
+        return produtoCores;
+    }
+
+    public void setProdutoCores(List<String> produtoCores) {
+        this.produtoCores = produtoCores;
+    }
+
+    public String getProdutoUrlMock() {
+        return produtoUrlMock;
+    }
+
+    public void setProdutoUrlMock(String produtoUrlMock) {
+        this.produtoUrlMock = produtoUrlMock;
+    }
+
+    public List<ComponentePojo> getCompoentes() {
+        return compoentes;
+    }
+
+    public void setCompoentes(List<ComponentePojo> compoentes) {
+        this.compoentes = compoentes;
+    }
+}
+
+
+
+
+
